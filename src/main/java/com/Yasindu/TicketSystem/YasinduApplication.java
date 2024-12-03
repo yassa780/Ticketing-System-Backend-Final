b6c8a96d0a921eb8e461920be6ba3fd83009ac91
yassa780 <141218490+yassa780@users.noreply.github.com>
2024-12-03 18:54:07 +0530

    Initial commit

---
+++ src/main/java/com/Yasindu/TicketSystem/YasinduApplication.java
@@ -0,0 +1,13 @@
+package com.Yasindu.TicketSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YasinduApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YasinduApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Yasindu/TicketSystem/YasinduApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Yasindu.TicketSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YasinduApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
